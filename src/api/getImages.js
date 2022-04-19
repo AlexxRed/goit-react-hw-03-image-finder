f199e5b9afc63a7557839edd9745e26187d749ed
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2022-04-19 18:53:57 +0300

    start

---
+++ src/api/getImages.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const BASIC_URL = 'https://pixabay.com/api/';
+const API_KEY = '5132282-75e364beaf68381714aa1df4d';
+
+const getImages = async searchQuery =>{
+    const response = axios.get()
+}
