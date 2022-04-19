5f42349ff2880a0da92111f3235a76bd0a4c5fbf
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2022-04-19 12:10:20 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      Image Finder v3.0
-    </div>
-  );
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    images: [],
+  }
+
+  render(){
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          textTransform: 'uppercase',
+          color: '#010101',
+        }}
+      >
+        Image Finder v3.0
+      </div>
+    );
+  }
 };
