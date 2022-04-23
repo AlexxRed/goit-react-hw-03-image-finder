2debac6575fed3f7f473867833005cb28574e83d
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-04-23 21:52:16 +0300

    Update getImages.js

--- src/api/getImages.js
+++ src/api/getImages.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const BASIC_URL = 'https://pixabay.com/api/';
 const API_KEY = '5132282-75e364beaf68381714aa1df4d';
+const per_page = 12;
+axios.defaults.baseURL = BASIC_URL
 
-const getImages = async searchQuery =>{
-    const response = axios.get()
+export const getImages = async (searchQuery, page) =>{
+    const response = axios.get(`?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`)
+    return response.data;
 }
